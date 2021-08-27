e40fe60a8e281dbed84d034f26cf63a7c63fbb36
CodingWatchCollector <a.d.begun@gmail.com>
2021-08-27 02:46:23 +0200

    update schema

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,48 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "chez-mman-qr-code",
+    siteUrl: 'https://chez-mman-site-mobile.netlify.app/',
+    title: `Chez M'man`,
+    description: `La carte de restaurant Chez M'man, 4 rue des Marronniers, Lyon 69002`
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Chez M'man, la carte`,
+        short_name: `Chez M'man`,
+        start_url: `/`,
+        icon: `src/images/icon.svg`
+      }
+    },
     'gatsby-transformer-remark',
-    "gatsby-plugin-netlify-cms",
+    'gatsby-plugin-netlify-cms',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `./src/assets`,
-        name: 'markdown-pages',
-      },
+        name: 'markdown-pages'
+      }
     },
-    "gatsby-plugin-image",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-image',
+    'gatsby-plugin-mdx',
+    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/'
       },
-      __key: "images",
+      __key: 'images'
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/'
       },
-      __key: "pages",
-    },
-  ],
-};
-
+      __key: 'pages'
+    }
+  ]
+}
