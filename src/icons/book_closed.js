853cf6917c817a9b436448d60d97d4ee80e4d192
CodingWatchCollector <a.d.begun@gmail.com>
2021-09-19 20:26:24 +0200

    css tweaks

--- src/icons/book_closed.js
+++ src/icons/book_closed.js
@@ -13,8 +13,8 @@ const Icon = ({ startColor, endColor, className }) => {
       clip-rule='evenodd'
     >
       <linearGradient id='book-gradient' x2='1' y2='1'>
-        <stop offset='0%' stop-color={startColor || 'hsl(0, 40%, 50%)'} />
-        <stop offset='100%' stop-color={endColor || 'hsl(0, 40%, 10%)'} />
+        <stop offset='0%' stop-color={startColor || 'hsl(0, 40%, 10%)'} />
+        <stop offset='100%' stop-color={endColor || 'hsl(0, 40%, 50%)'} />
       </linearGradient>
       <path d='M5.495 2h16.505v-2h-17c-1.657 0-3 1.343-3 3v18c0 1.657 1.343 3 3 3h17v-20h-16.505c-1.375 0-1.375-2 0-2zm.505 4h14v16h-14v-16z' />
     </svg>
